4685cc845a65157cd3280d03fd2d2730b4bf5452
Aidan Butler <aidan.butler@azdigital-health.com>
2023-12-07 15:53:48 +0100

    Add day 7 part 2

--- 7/solve.py
+++ 7/solve.py
@@ -96,7 +96,9 @@ CARD_DICT = {
     "8": 8,
     "9": 9, 
     "T": 10,
-    "J": 11,
+    # J = 1 to solve part 2; J = 11 to solve part 1
+    #"J": 11,
+    "J": 1,
     "Q": 12,
     "K": 13,
     "A": 14,
@@ -131,13 +133,13 @@ class Cards:
         for i in range(len(self.cards)):
             self_val = self.cards[i]
             other_val = other.cards[i]
-            print(f"self_val = {self_val}")
-            print(f"other_val = {other_val}")
+            #print(f"self_val = {self_val}")
+            #print(f"other_val = {other_val}")
             if self_val < other_val:
-                print(f"{self_val} < {other_val}")
+                #print(f"{self_val} < {other_val}")
                 return True
             elif self_val > other_val:
-                print(f"{self_val} > {other_val}")
+                #print(f"{self_val} > {other_val}")
                 return False
         raise ValueError(f"Identical cards: {self} -- {other}")
 
@@ -185,10 +187,10 @@ class Typ:
         elif len(set(l)) == 2 and 3 in frequencies:
             return "full_house"
         else:
-            print(f"type(l) = {type(l)}")
-            print(f"count_dict = {count_dict}")
-            print(f"card_set = {card_set}")
-            print(f"frequencies = {frequencies}")
+            #print(f"type(l) = {type(l)}")
+            #print(f"count_dict = {count_dict}")
+            #print(f"card_set = {card_set}")
+            #print(f"frequencies = {frequencies}")
             raise ValueError(str(l))
 
     def __str__(self):
@@ -227,6 +229,57 @@ class Hand:
         return str(self)
 
 
+class JokerHand(Hand):
+    def __init__(self, s: str, index: int):
+        super().__init__(s, index)
+        has_joker = "J" in self.raw_cards
+        if has_joker:
+            self.best_hand = self._get_best_possible_hand()
+        else:
+            #print(f"self = {self}")
+            self.best_hand = self
+            #print(f"self.best_hand = {self.best_hand}") 
+
+    def _get_best_possible_hand(self) -> Hand:
+        print(f"self.raw_cards = {self.raw_cards}")
+        all_lists = []
+        get_joker_hands(self.raw_cards, all_lists,[])
+        #print(f"all_lists = {all_lists}")
+        for item in all_lists:
+            assert "J" not in item
+        possible_hands = [JokerHand(f"{''.join(item)} {self.bid}", self.index) for item in all_lists]
+        #print(f"possible_hands = {possible_hands}")
+        max_possible_hand = max(possible_hands)
+        print(f"max_possible_hand = {max_possible_hand}")
+        return max_possible_hand
+
+    def __lt__(self, other) -> bool:
+        if self.best_hand.type < other.best_hand.type:
+            return True
+        elif self.best_hand.type > other.best_hand.type:
+            return False
+        else:
+            if self.cards < other.cards:
+                return True
+            elif self.cards > other.cards:
+                return False
+            else:
+                raise ValueError(f"Identical hands: {self}, {other}")            
+            
+
+def get_joker_hands(l: List[str], all_lists: List[str], current_list: List[str], card_dict: Iterable[str] = CARD_DICT) -> List[str]:
+    if len(l) == 0:
+        all_lists += [current_list]
+        return
+    next_item = l[0]
+    if next_item == "J":
+        for card in card_dict:
+            if card != "J":
+                get_joker_hands(l[1:], all_lists, current_list+[card], card_dict)
+    else:
+        get_joker_hands(l[1:], all_lists, current_list+[next_item], card_dict)
+
+
 def get_count_dict(l: Iterable[Any]) -> Dict[Any, int]:
     count_dict = {}
     for item in l:
@@ -253,14 +306,31 @@ def solve(input_string: str) -> List[int]:
     return result
 
 
+def solve2(input_string: str) -> List[int]:
+    result = None
+    raw_list = input_string.split("\n")
+    raw_list = [l.strip() for l in raw_list]
+    cleaned_list = [item for item in raw_list if len(item) > 0] 
+    print(f"cleaned_list = {cleaned_list}")
+    hands = [JokerHand(s, i) for i, s in enumerate(cleaned_list)]
+    print(f"hands = {hands}")
+    sorted_hands = sorted(hands)
+    print(f"sorted_hands = {sorted_hands}")
+    multiplied_hands = [hand.bid * (i + 1) for i, hand in enumerate(sorted_hands)]  
+    print(f"multiplied_hands = {multiplied_hands}")
+    result = sum(multiplied_hands)
+    return result
 
-print(solve(TEST_INPUT))
+
+#print(solve(TEST_INPUT))
         
-with open("input.txt", "r") as f:
-    print(solve(f.read()[:-1]))
-
-#print(solve2_cython(TEST_INPUT))
-#        
 #with open("input.txt", "r") as f:
-#    print(solve2_cython(f.read()[:-1]))
+#    print(solve(f.read()[:-1]))
 
+print(solve2(TEST_INPUT))
+#        
+with open("input.txt", "r") as f:
+    print(solve2(f.read()[:-1]))
+
+
+# notes 253718982 too low
