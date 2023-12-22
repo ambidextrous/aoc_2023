221a2cab0049c671ba90012933734acd6ab89e0b
Aidan Butler <aidan.butler@azdigital-health.com>
2023-12-22 13:24:50 +0100

    WIP: implimented falling

--- 22/solve.py
+++ 22/solve.py
@@ -59,20 +59,39 @@ class Brick:
                     units += (unit,)
         self.units = units
         self.volume = len(self.units)
-        self.under = None
+        self.under = []
+
+    def on_ground(self) -> bool:
+        return any([u.z == 0 for u in self.units])
 
     def occupies(self, x: int, y: int, z: int) -> bool:
         return any([u.occupies(x,y,z) for u in self.units])
 
-    def get_bricks_under(self, brick_dict: Tuple[Any]):
+    def set_bricks_under(self, brick_dict: Tuple[Any]):
         under = []
+        min_z = min([u.z for u in self.units])
         for u in self.units:
-            for z in range(0,u.x):
+            for z in range(0,min_z):
                 coord = (u.x, u.y, z)
                 if coord in brick_dict:
                     under += [brick_dict[coord]]
         under.reverse()
         self.under = under
+
+    def try_falling(self) -> bool:
+        is_on_ground = self.on_ground()
+        is_supported = any([self.is_supported_by(b) for b in self.under]) if len(self.under) > 0 else False
+        if is_on_ground or is_supported:
+            return False
+        else:
+            self.starts = (self.starts[0], self.starts[1], self.starts[2]-1)
+            self.ends = (self.ends[0], self.ends[1], self.ends[2]-1)
+            units = []
+            for u in self.units:
+                new_unit = Unit(u.x, u.y, u.z-1)
+                units += [new_unit]
+            self.units = units
+            return True
 
     def is_supported_by(self, other) -> bool:
         for u in self.units:
@@ -82,7 +101,7 @@ class Brick:
         return False
  
     def __str__(self):
-        return f"Brick: [{self.num}] start={self.starts}; end={self.ends}; volume={self.volume} units={self.units}"
+        return f"Brick: [{self.num}] start={self.starts}; end={self.ends}; under = {[b.num for b in self.under]}; volume={self.volume}; units={self.units}"
 
     def __repr__(self):
         return str(self)
@@ -122,6 +141,7 @@ def print_bricks(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> Non
             line += ch
         print(line + f" {z}")
 
+
 def get_limits(bricks: List[Brick]) -> Tuple[int]:
     x_max = 0
     y_max = 0
@@ -139,6 +159,21 @@ def get_limits(bricks: List[Brick]) -> Tuple[int]:
     return x_max, y_max, z_max
 
 
+def fall(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> List[Brick]:
+    i = 0
+    while True:
+        something_fell = False
+        i += 1
+        for b in bricks:
+            brick_fell = b.try_falling()
+            if brick_fell:
+                something_fell = True
+        print(i)
+        print_bricks(bricks, x_max, y_max, z_max)
+        if something_fell is False:
+            return bricks
+         
+
 def solve(input_string: str) -> List[int]:
     result = None
     raw_list = input_string.split("\n")
@@ -148,12 +183,15 @@ def solve(input_string: str) -> List[int]:
     bricks = [Brick(l, i) for i, l in enumerate(cleaned_list)]
     print(f"bricks = {bricks}")
     x_max, y_max, z_max = get_limits(bricks)
-    print_bricks(bricks, x_max, y_max, z_max)
     print(f"x_max = {x_max}, y_max = {y_max}, z_max = {z_max}")
-    brick_dict = {b.num: b for b in bricks}
-    print(f"brick_dict = {brick_dict}")
     occupation_dict = get_occupation_dict(bricks, x_max, y_max, z_max)
     print(f"occupation_dict = {occupation_dict}")
+    for b in bricks:
+        print(f"    {b}")
+        b.set_bricks_under(occupation_dict)
+    print(f"bricks = {bricks}")
+    print_bricks(bricks, x_max, y_max, z_max)
+    fallen_bricks = fall(bricks, x_max, y_max, z_max)
 
     return result
 
