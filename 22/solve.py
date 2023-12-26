ddaf276ae128b0b33ac5cb07e37de03139a17ca4
Aidan Butler <aidan.butler@azdigital-health.com>
2023-12-26 20:48:29 +0000

    Clean up code

--- 22/solve.py
+++ 22/solve.py
@@ -20,310 +20,118 @@ from math import inf
 
 sys.setrecursionlimit(100000)
 
-class Unit:
-    def __init__(self, x: int, y: int, z: int):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.below = None
-        self.above = None
 
-    def occupies(self, x: int, y:int, z: int) -> bool:
-        return x == self.x and y == self.y and z == self.z
+def parse_brick(s: str, num: int) -> Tuple[Tuple[Any]]:
+    start = [int(item) for item in s.split("~")[0].split(",")]
+    end = [int(item) for item in s.split("~")[1].split(",")]
+    brick = tuple()
+    for x in range(start[0],end[0]+1):
+       for y in range(start[1],end[1]+1):
+           for z in range(start[2],end[2]+1):
+               brick += ((x, y, z, num, "active"),)
+    return brick
 
-    def __str__(self):
-        return f"Unit: ({self.x},{self.y},{self.z})"
-
-    def __repr__(self):
-        return str(self)
-
-    def __eq__(self, other):
-        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
-
-    def __lt__(self, other):
-        return (self.x, self.y, self.z) < (other.x, other.y, other.z)
-  
-
-class Brick:
-    def __init__(
-        self, 
-        s: str,
-        num: int,
-    ):
-        starts = [int(item) for item in s.split("~")[0].split(",")]
-        self.starts = starts
-        self.num = num
-        ends = [int(item) for item in s.split("~")[1].split()[0].split(",")]
-        self.ends = ends
-        for i in range(len(starts)):
-            assert starts[i] <= ends[i], s
-        units = ()
-        z_min = inf
-        z_max = -inf
-        for x in range(starts[0], ends[0]+1): 
-            for y in range(starts[1], ends[1]+1):
-                for z in range(starts[2], ends[2]+1):
-                    if z < z_min:
-                        z_min = z
-                    if z > z_max:
-                        z_max = z
-                    unit = Unit(x,y,z)
-                    units += (unit,)
-        self.units = units
-        self.volume = len(self.units)
-        self.under = []
-        self.bottom = [u for u in self.units if u.z == z_min]
-        self.top = [u for u in self.units if u.z == z_max]
-        self.z_min = z_min
-        self.z_max = z_max
-        self.is_frozen = False
-
-    def on_ground(self) -> bool:
-        return any([u.z == 0 for u in self.units])
-
-    def occupies(self, x: int, y: int, z: int) -> bool:
-        return any([u.occupies(x,y,z) for u in self.units])
-
-    def __lt__(self, other):
-        return self.z_min < other.z_min
-
-    def set_bricks_under(self, brick_dict: Tuple[Any]):
-        under = []
-        min_z = min([u.z for u in self.units])
-        for u in self.units:
-            for z in range(0,min_z):
-                coord = (u.x, u.y, z)
-                if coord in brick_dict:
-                    under += [brick_dict[coord]]
-        under.reverse()
-        self.under = list(set(under))
-
-    def get_aboves(self, bricks: List[Any]) -> List[Any]:
-        aboves = []
-        for b in bricks:
-            for u in self.units:
-                for u_1 in b.units:
-                    if b.num != self.num and u.x == u_1.x and u.y == u_1.y and u.z == u_1.z-1:
-                        aboves += [b]
-        return aboves
-
-    def get_belows(self, bricks: List[Any]) -> List[Any]:
-        aboves = []
-        for b in bricks:
-            for u in self.units:
-                for u_1 in b.units:
-                    if b.num != self.num and u.x == u_1.x and u.y == u_1.y and u.z == u_1.z+1:
-                        aboves += [b]
-        return aboves
-
-    def try_falling(self) -> bool:
-        is_on_ground = self.on_ground()
-        is_supported = any([self.is_supported_by(b) for b in self.under]) if len(self.under) > 0 else False
-        if is_on_ground or is_supported:
-            return False
+def freeze(bricks: List[Tuple[Any]], frozen: Tuple[Tuple[Any]]) -> Tuple[Tuple[Tuple[Any]], Tuple[Tuple[Any]]]:
+    active = tuple()
+    for brick in bricks:
+        is_frozen = False
+        bottom_z = min([unit[2] for unit in brick])
+        bottom_units = [unit for unit in brick if unit[2] == bottom_z]
+        if bottom_z == 1:
+            is_frozen = True
         else:
-            self.starts = (self.starts[0], self.starts[1], self.starts[2]-1)
-            self.ends = (self.ends[0], self.ends[1], self.ends[2]-1)
-            units = []
-            for u in self.units:
-                new_unit = Unit(u.x, u.y, u.z-1)
-                units += [new_unit]
-            self.units = units
-            return True
-
-    def is_supported_by(self, other) -> bool:
-        for u in self.units:
-            for u_1 in other.units:
-                if u.z - u_1.z == 1:
-                    return True
-        return False
- 
-    def __str__(self):
-        return f"Brick: [{self.num}] volume={self.volume}; top = {self.top}; bottom = {self.bottom}; units={self.units}"
-
-    def __repr__(self):
-        return str(self)
+            for unit in bottom_units:
+                for b in frozen:
+                    for u in b:
+                        if unit[0] == u[0] and unit[1] == u[1] and unit[2] - 1 == u[2]:
+                            is_frozen = True
+        if is_frozen:
+            brick = tuple((u[0], u[1], u[2], u[3], "frozen") for u in brick)
+            frozen += (brick,)
+        else:
+            active += (brick,)
+    return frozen, active
 
 
-def get_occupation_dict(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> Dict[Tuple[int], Brick]:
-    d = {}
-    for x in range(x_max+1):
-        for y in range(y_max+1):
-            for z in range(z_max+1):
-                for b in bricks:
-                    if b.occupies(x, y, z):
-                        d[(x,y,z)] = b
-    return d
-    
-
-def print_bricks(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> None:
-    print("x")
-    occupation_dict = get_occupation_dict(bricks, x_max, y_max, z_max)
-    for z in range(z_max,-1,-1):
-        line = ""
-        for x in range(0,x_max+1):
-            ch = "."
-            for y in range(0,z_max+1):
-                if (x, y, z) in occupation_dict:
-                    ch = "#"
-            line += ch
-        print(line + f" {z}")
-    print("y")
-    for z in range(z_max,-1,-1):
-        line = ""
-        for y in range(0,y_max+1):
-            ch = "."
-            for x in range(0,x_max+1):
-                if (x, y, z) in occupation_dict:
-                    ch = "#"
-            line += ch
-        print(line + f" {z}")
-
-
-def get_limits(bricks: List[Brick]) -> Tuple[int]:
+def get_maxes(bricks: List[Tuple[Any]]) -> Tuple[int]:
     x_max = 0
     y_max = 0
     z_max = 0
-    for b in bricks:
-        b_x = b.ends[0]
-        b_y = b.ends[1]
-        b_z = b.ends[2]
-        if b_x > x_max:
-            x_max = b_x 
-        if b_y > y_max:
-            y_max = b_y 
-        if b_z > z_max:
-            z_max = b_z 
+    for brick in bricks:
+        for unit in brick:
+            x = unit[0]
+            y = unit[1]
+            z = unit[2]
+            if x > x_max:
+                x_max = x
+            if y > y_max:
+                y_max = y
+            if z > z_max:
+                z_max = z
     return x_max, y_max, z_max
 
 
-def fall(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> List[Brick]:
-    i = 0
-    while True:
-        something_fell = False
-        i += 1
-        for b in bricks:
-            brick_fell = b.try_falling()
-            if brick_fell:
-                something_fell = True
-        #print(i)
-        #print_bricks(bricks, x_max, y_max, z_max)
-        if something_fell is False:
-            return bricks
-         
-
-def get_disints(bricks: List[Brick]) -> List[Brick]:
-    disints = []
-    for brick in bricks:
-        print()
-        print(f"brick = {brick}")
-        aboves = brick.get_aboves(bricks)
-        print(f"aboves = {aboves}")
-        if len(aboves) == 0:
-            disints += [brick]
-        else:
-            belows_of_aboves = [b.get_belows(bricks) for b in aboves]
-            print(f"belows_of_aboves = {belows_of_aboves}")
-            lengths = [len(item) for item in belows_of_aboves]
-            print(f"lengths = {lengths}")
-            if min(lengths) >= 2:
-                disints += [brick]
-        print(f"distints = {[b.num for b in disints]}")
-    return disints
+def print_bricks(bricks: Tuple[Tuple[Any]], x_max: int, y_max: int, z_max: int) -> None:
+    print("y:")
+    for z in range(z_max,-1,-1):
+        l = ""
+        for y in range(y_max+1):
+            char = "."
+            for x in range(x_max+1):
+                for brick in bricks:
+                    for unit in brick:
+                        if unit[0] == x and unit[1] == y and unit[2] == z:
+                            char = "#"
+            l += char
+        print(f"{l} : {z}")
+    print("x:")
+    for z in range(z_max,-1,-1):
+        l = ""
+        for x in range(x_max+1):
+            char = "."
+            for y in range(y_max+1):
+                for brick in bricks:
+                    for unit in brick:
+                        if unit[0] == x and unit[1] == y and unit[2] == z:
+                            char = "#"
+            l += char
+        print(f"{l} : {z}")
 
 
-def clean_dict(occupation_dict: Tuple[Tuple[int], Brick], brick: Brick):
-    d = deepcopy(occupation_dict)
-    print([k for k in d])
-    print(brick.units)
-    for u in brick.units:
-        d.pop((u.x, u.y, u.z))
-    return d
-
-def test_fall(bricks: List[Brick], x_max: int, y_max: int, z_max: int) -> int:
-    occupation_dict = get_occupation_dict(bricks, x_max, y_max, z_max)
-    count = 0
-    for i in range(1,len(bricks)):
-        print(f"i = {i}; {(i+1)/len(bricks)}")
-        removed_brick = bricks[i]
-        bs = deepcopy(bricks)
-        new_bricks = bs[:i-1]+bs[i:]
-        for b in new_bricks:
-            b.set_bricks_under(occupation_dict)
-        copied_bricks = deepcopy(new_bricks)
-        fallen = fall(copied_bricks, x_max, y_max, z_max)
-        new_starts = sorted([n.units for n in new_bricks])
-        fallen_starts = sorted([n.units for n in fallen])
-        #print(f"len(new_starts) = {len(new_starts)}")
-        #print(f"new_starts = {new_starts}")
-        #print(f"fallen_starts = {fallen_starts}")
-        is_match = new_starts == fallen_starts
-        if is_match:
-            count += 1
-    return count
+def drop(active: Tuple[Tuple[Any]], frozen: Tuple[Any]) -> Tuple[Any]:
+    counter = 0
+    while len(active) > 0:
+        counter += 1
+        print(f"dropping, iteration {counter}")
+        dropped_active = tuple()
+        for brick in active:
+            new_brick = tuple()
+            for unit in brick:
+                new_unit = (unit[0], unit[1], unit[2]-1, unit[3], unit[4])
+                new_brick += (new_unit,)
+            dropped_active += (new_brick,)
+        new_frozen, new_active = freeze(dropped_active, frozen) 
+        active = new_active
+        frozen = new_frozen
+    return frozen
 
 
-class Floor:
-    def __init__(self, x_max: int, y_max: int):
-        self.x_max = x_max
-        self.y_max = y_max
-        self.set_zs(0)
-
-    def raise_tile(self, x: int, y: int, z: int) -> None:
-        print(f"raise_tile: x={x}; y={y}; z={z}")
-        current_z = self.top[x][y].z
-        if z < current_z:
-            raise ValueError(f"x={x}; y={y}; z={z}; floor={floor}")
-        self.top[x][y].z = z
-
-    def set_zs(self, z: int):
-        top = [Unit(x, y, z) for x in range(self.x_max+1) for y in range(self.y_max+1)]
-        outter_dict = {x: {} for x in range(self.x_max+1)}
-        for u in top:
-            outter_dict[u.x][u.y] = u
-        self.top = outter_dict
-                
-                
-       
-    def is_touched_by(self, brick: Brick) -> bool:
-        for u in brick.bottom:
-            print(f"u = {u}")
-            print(f"self.top = {self.top}")
-            u_1 = self.top[u.x][u.y]
-            print(f"u_1 = {u_1}")
-            if u.x == u_1.x and u.y == u_1.y and u.z == u_1.z + 1:
-                return True
-        return False
-
-    def __str__(self):
-        return f"Floor = {self.top}"
- 
-    def __repr__(self):
-        return str(self)
- 
-
-def freeze(bricks: List[Brick], floor: Floor) -> Tuple[List[Brick], List[Brick], Floor]:
-    frozen = []
-    active = []
-    floor = deepcopy(floor)
-    for i, brick in enumerate(bricks):
-        print(f"i = {i}")
-        print(f"brick = {brick}")
-        if floor.is_touched_by(brick):
-            print(f"brick.units = {brick.units}")
-            for u in brick.units:
-                print(f"u = {u}")
-                print(f"u.x = {u.x}")
-                floor.raise_tile(u.x, u.y, u.z)
-            frozen += [brick]
-        else:
-            active += [brick]
-    return active, frozen, floor
-
-    
-def drop(bricks: List[Brick], floor: Floor) -> List[Brick]:
-    frozen = []
-    active = []
+def disint(bricks: Tuple[Tuple[Any]]) -> int:
+    disintigrable_counter = 0
+    chain_reaction_counter = 0
+    for i in range(len(bricks)):
+        print(f"i = {i}: {i/len(bricks)}; counter {disintigrable_counter}")
+        temp_pop = bricks[i]
+        before = bricks[:i]
+        after = bricks[i+1:]
+        new_bricks = before + after
+        new_frozen, new_active = freeze(new_bricks, tuple())
+        print(f"len(new_frozen) = {len(new_frozen)}")
+        print(f"len(new_active) = {len(new_active)}")
+        if len(new_active) > 0:
+            disintigrable_counter += 1
+        chain_reaction_counter += len(new_active)
+    return disintigrable_counter, chain_reaction_counter
 
 
 def solve(input_string: str) -> List[int]:
@@ -332,20 +140,19 @@ def solve(input_string: str) -> List[int]:
     raw_list = [l for l in raw_list]
     cleaned_list = [item.replace("\n","") for item in raw_list if len(item) > 0] 
     print(f"cleaned_list = {cleaned_list}")
-    bricks = sorted([Brick(l, i) for i, l in enumerate(cleaned_list)])
+    bricks = sorted([parse_brick(s, i) for i, s in enumerate(cleaned_list)], key=lambda x: min([y[2] for y in x]))
     print(f"bricks = {bricks}")
-    x_max, y_max, z_max = get_limits(bricks)
-    floor = Floor(x_max, y_max) 
-    print(f"floor = {floor}")
-    active, frozen, floor = freeze(bricks, floor)
-    print(f"active = {active}")
-    print(f"len(active) = {len(active)}")
-    print(f"frozen = {frozen}")
-    print(f"len(frozen) = {len(frozen)}")
-    print(f"floor = {floor}")
+    x_max, y_max, z_max = get_maxes(bricks)
+    print(f"x_max = {x_max}; y_max = {y_max}; z_max = {z_max}")
+    frozen, active = freeze(bricks, tuple())
+    dropped = drop(active, frozen)
+    print(f"dropped = {dropped}")
+    print("dropped:")
+    print_bricks(dropped, x_max, y_max, z_max)
+    non_disintigrable_count, chain_reaction_count = disint(dropped)
+    disintigrable_count = len(bricks) - non_disintigrable_count
 
-
-    return result
+    return disintigrable_count, chain_reaction_count
 
 
 
@@ -359,4 +166,3 @@ print(solve(TEST_INPUT))
 #with open("input.txt", "r") as f:
 #    print(solve22(f.read()[:-1]))
 
-# 670 is too high
