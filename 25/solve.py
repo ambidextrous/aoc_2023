bff6cdec63c33bc1e8d7e7c576b8ad2036f5d82b
Aidan Butler <aidan.butler@azdigital-health.com>
2024-01-07 08:17:12 +0100

    Add day 25 part 2.

--- 25/solve.py
+++ 25/solve.py
@@ -29,8 +29,29 @@ import sympy as sp
 from typing import Tuple
 import igraph as ig
 import matplotlib.pyplot as plt
+import networkx as nx
+import random
+from networkx.algorithms.connectivity import minimum_st_edge_cut
+from igraph import Graph
+from collections import defaultdict, deque
+
 
 sys.setrecursionlimit(10000)#0)
+
+def find_cut_edges(edges: Tuple[Tuple[str]], nodes: Tuple[str]) -> Tuple[str]:
+    graph = nx.Graph(edges)
+    # Find the cut edges (bridges) in the graph
+    #min_cut_edges = list(nx.k_edge_components(graph,k=3))
+    #print(f"len(min_cut_edges) = {len(min_cut_edges)}")
+    #return min_cut_edges
+    while True:
+        edge_1 = random.choice(nodes)
+        edge_2 = random.choice(nodes)
+        min_cut_edges = list(minimum_st_edge_cut(graph,edge_1,edge_2))
+        sorted_edges = [tuple(sorted(list(edge))) for edge in min_cut_edges]
+        print(f"sorted_edges = {sorted_edges}")
+        if len(sorted_edges) == 3:
+            return tuple(sorted_edges)
 
 
 def visualize_undirected_graph(nodes: Tuple[str], edges: Tuple[Tuple[str]]) -> None:
@@ -45,7 +66,7 @@ def visualize_undirected_graph(nodes: Tuple[str], edges: Tuple[Tuple[str]]) -> N
 
     # Plot the graph
     layout = g.layout("circle")
-    ig.plot(g, layout=layout, bbox=(900, 900), vertex_label=nodes, target='part_1.pdf')
+    ig.plot(g, layout=layout, bbox=(1200, 1200), vertex_label=nodes, target='part_1.pdf')
     #plt.show()
 
 
@@ -118,7 +139,7 @@ def unplug(nodes: Tuple[str], edges: Tuple[Tuple[str]]) -> int:
     counter = 0
     for config in configuations:
         counter += 1
-        if counter % 1000 == 0:
+        if counter % 100 == 0:
             print(f"counter = {counter}")
         node_dict = generate_node_dict(nodes, config)
         group_size_dict = count_groups(nodes, node_dict)
@@ -129,6 +150,62 @@ def unplug(nodes: Tuple[str], edges: Tuple[Tuple[str]]) -> int:
         #print(f"group_size_dict = {group_size_dict}")
 
 
+def min_unplug(nodes: Tuple[str], edges: Tuple[Tuple[str]], min_cut_edges: Tuple[Tuple[str]]) -> int:
+    cleaned_edges = [edge for edge in edges if edge not in min_cut_edges]
+    node_dict = generate_node_dict(nodes, cleaned_edges)
+    group_size_dict = count_groups(nodes, node_dict)
+    #print(f"cleaned_edges = {cleaned_edges}")
+    print(f"group_size_dict = {group_size_dict}")
+    return group_size_dict[0] * group_size_dict[1]
+
+
+def group_by_unique_paths(data: List[str]):
+    components_map = defaultdict(set)
+
+    # mapping components to components
+    for line in data:
+        l, r = line.strip().split(': ')
+        r = r.split(' ')
+        for comp in r:
+            components_map[l].add(comp)
+            components_map[comp].add(l)
+
+    group_1 = 1
+    group_2 = 0
+
+    first_comp = list(components_map.keys())[0]
+
+    for component in list(components_map.keys())[1:]:
+        connections = 0
+        used_components = {first_comp}
+        # Find shortest path for considered component
+        # for each of starting component without repeating used components
+        for s_component in components_map[first_comp]:
+            if s_component == component:
+                connections += 1
+                continue
+            qed = set()
+            q = deque()
+            q.append((s_component, [s_component]))
+            found = False
+            while q and not found and connections < 4:
+                comp, path = q.popleft()
+                for c in components_map[comp]:
+                    if component == c:
+                        connections += 1
+                        used_components.update(path)
+                        found = True
+                        break
+                    elif c not in qed and c not in path and c not in used_components:
+                        q.append([c, path + [c]])
+                        qed.add(c)
+        # If it finds more than 3 unique ways to get to given component then it is in group 1
+        if connections >= 4:
+            group_1 += 1
+        else:
+            group_2 += 1
+
+    return group_1 * group_2    
 
 def solve(input_string: str) -> List[int]:
     result = None
@@ -141,16 +218,16 @@ def solve(input_string: str) -> List[int]:
     edges = parse_edges(cleaned_list)
     print(f"edges = {edges}")
     visualize_undirected_graph(nodes, edges)
-    result = unplug(nodes, edges)
+    result = group_by_unique_paths(cleaned_list)
 
     return result
 
 
 
-print(solve(TEST_INPUT))
+#print(solve(TEST_INPUT))
         
-#with open("input.txt", "r") as f:
-#    print(solve(f.read()[:-1]))
+with open("input.txt", "r") as f:
+    print(solve(f.read()[:-1]))
 
 #print(solve2(TEST_INPUT))
 
